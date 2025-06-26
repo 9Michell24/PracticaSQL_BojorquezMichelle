0181e8ebb6e859581ee2ac59a1521307d82e1616
michelle <michellebojorquezsterling@gmail.com>
2025-06-26 15:12:54 -0700

    formulario

---
+++ PracticaSQL_BojorquezMichelle/app/src/main/java/mx/edu/potros/practicasql_bojorquezmichelle/Alumno.kt
@@ -0,0 +1,4 @@
+package mx.edu.potros.practicasql_bojorquezmichelle
+
+data class Alumno( val nombre: String, val apPaterno: String, val apMaterno: String, val carrera: String
+)
---
+++ PracticaSQL_BojorquezMichelle/app/src/main/java/mx/edu/potros/practicasql_bojorquezmichelle/MainActivity.kt
@@ -0,0 +1,44 @@
+package mx.edu.potros.practicasql_bojorquezmichelle
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.EditText
+import android.util.Log
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val etNombre = findViewById<EditText>(R.id.nombre)
+        val etApPat = findViewById<EditText>(R.id.apPat)
+        val etApMat = findViewById<EditText>(R.id.apMat)
+        val etCarrera = findViewById<EditText>(R.id.carrera)
+        val btnGuardar = findViewById<Button>(R.id.btnGuardar)
+
+        btnGuardar.setOnClickListener {
+            val nombre = etNombre.text.toString().trim()
+            val apPat = etApPat.text.toString().trim()
+            val apMat = etApMat.text.toString().trim()
+            val carrera = etCarrera.text.toString().trim()
+
+            if (nombre.isEmpty() || apPat.isEmpty() || apMat.isEmpty() || carrera.isEmpty()) {
+                Toast.makeText(this, "Por favor, llena todos los campos", Toast.LENGTH_SHORT).show()
+            } else {
+                val alumno = Alumno(
+                    nombre = nombre,
+                    apPaterno = apPat,
+                    apMaterno = apMat,
+                    carrera = carrera
+                )
+                Toast.makeText(this, "Alumno guardado", Toast.LENGTH_SHORT).show()
+                Log.d("MainActivity", "Alumno guardado: $alumno")
+            }
+        }
+    }
+}
